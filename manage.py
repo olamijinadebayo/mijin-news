4aab90705f78ec707cdda829d60bf224cf464041
olamijinadebayo <consultolamijin@gmail.com>
2018-04-08 23:59:50 +0300

    this will add commands which willl enable us in running the program

---
+++ manage.py
@@ -0,0 +1,20 @@
+from app import create_app
+from flask_script import Manager,Server
+
+# creating app instance
+app = create_app('development')
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+@manager.add_command
+def test():
+    '''
+    this will rin unittests
+    '''
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+if __name__ == '__main__':
+    manager.run()    
