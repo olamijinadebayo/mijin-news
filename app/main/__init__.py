032865be31614c6e41a021ae61348034099c78d1
olamijinadebayo <consultolamijin@gmail.com>
2018-04-09 00:34:55 +0300

    this will add the news api key

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,3 +1,3 @@
-from flask import blueprint
+from flask import Blueprint
 main = Blueprint('main',__name__)
 from . import views,errors
---
+++ start.sh
@@ -0,0 +1,3 @@
+export NEWS_API_KEY='0814a0a6826142e5ba6c15f332872797'
++
++python3.6 manage.py server
