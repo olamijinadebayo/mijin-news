3ddd50df670deb0a6172762dd76b8820b4d743c7
olamijinadebayo <consultolamijin@gmail.com>
2018-04-08 23:44:48 +0300

    this will initialze the flask app

---
+++ app/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+
+    # initializing application
+    app = Flask(__name__)
+
+    #creating the app config_options
+    app.config.from_object(config_options[config_name])
+
+    # initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Reqistering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # Setting config
+    from .requests import configure_request
+    configure_request(app)
+
+    return app
