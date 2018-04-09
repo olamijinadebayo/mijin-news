c993e64c07dfbfce5442e3ebf356224cdca0b501
olamijinadebayo <consultolamijin@gmail.com>
2018-04-09 21:22:25 +0300

    this will add the functions to get articles in json format and display them in a list

--- app/requests.py
+++ app/requests.py
@@ -34,3 +34,28 @@ def process_sources(source_list):
         source_results.append(source_object)
 
     return source_results
+def get_articles(id):
+    get_articles_url = 'https://newsapi.org/v1/articles?source={}&apiKey={}'.format(id,api_key)
+    with urllib.request.urlopen(get_articles_url) as url:
+        get_articles_data = url.read()
+        get_articles_response = json.loads(get_articles_data)
+
+        articles_results = None
+
+        if get_articles_response['articles']:
+            articles_results_list = get_articles_response['articles']
+            articles_results = process_articles(articles_results_list)
+    return articles_results
+
+def process_articles(article_list):
+    article_results = []
+
+    for article_item in article_list:
+        source = article_item.get('source')
+        title = article_item.get('title')
+        urlToImage = article_item.get('urlToImage')
+        description = article_item.get('description')
+        urlToArticle = article_item.get('url')
+        publishedAt = article_item.get('publishedAt')
+
+    return article_results
