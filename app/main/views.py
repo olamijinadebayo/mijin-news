90a79fc75b47aa70e7032330a8315cac4284e4fd
olamijinadebayo <consultolamijin@gmail.com>
2018-04-09 21:06:00 +0300

    this will add the view function for the index page

---
+++ app/main/views.py
@@ -0,0 +1,20 @@
+from flask import render_template,request,redirect,url_for
+from . import main
+from ..requests import get_sources,get_articles
+
+@main.route('/')
+def index():
+    '''
+    the view function that returns the home page with its information
+    '''
+    title = "Welcome To MijinNews "
+    sources = get_sources()
+    return render_template('index.html', title = title,sources = sources)
+
+@main.route('/source/<source_id>')
+def source(source_id):
+    '''
+    view function that returns the sources
+    '''
+    title = f"{source_id} page"
+    return render_template('source.html',title = title,articles =articles)
